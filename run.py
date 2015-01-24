fdb4bf6eecf78f0a39b928222bc61033b12e3a25
Arne Schauf <git@as-webservices.de>
2015-01-24 01:39:07 +0100

    small improvements

--- run.py
+++ run.py
@@ -42,6 +42,7 @@ class StdOutListener(tweepy.streaming.StreamListener):
     """
 
     last_own_tweet = 0
+    min_tweet_secs = 20
     questions = []
 
     def on_data(self, data):
@@ -52,17 +53,22 @@ class StdOutListener(tweepy.streaming.StreamListener):
         if not data.get('in_reply_to_screen_name') and\
                 not data.get('in_reply_to_status_id') and\
                 not data.get('in_reply_to_user_id') and\
-                self.last_own_tweet + 5 < time.time():
+                self.last_own_tweet + self.min_tweet_secs < time.time():
             if text[-1:] == '?' and text.lower().startswith('why'):
                 self.questions.append({
                     'username': username,
                     'id': data.get('id'),
                     'text': text
                 })
-            elif 'because' in text.lower() and len(self.questions) > 0:
-                answer = text[text.lower().find('because'):]
+            elif u'because' in text.lower() and\
+                    len(self.questions) > 0 and\
+                    not text[-1:] == u'…' and\
+                    u'http' not in text and\
+                    u'@' not in text and\
+                    len(text) < 110:
+                answer = text[text.lower().find('because'):].replace(' me ', ' you ')
                 qdata = self.questions.pop()
-                print("Answering: " + qdata['text'])
+                print("Answering @{user}: {text}".format(user=qdata['username'], text=qdata['text']))
                 print(answer)
                 print('####################')
                 status = u'@{user} {answer}'.format(user=qdata['username'], answer=answer)
@@ -75,29 +81,37 @@ class StdOutListener(tweepy.streaming.StreamListener):
             answer = "I'm a bot automatically answering to all questions on twitter. Sorry for confusion! {t}".\
                 format(t=str(time.time()))
             print('####################')
-            print('Got answer from @{user}: {answer}'.format(user=username, answer=text))
-            print('Replying: ' + answer)
+            print(u'Got answer from @{user}: {answer}'.format(user=username, answer=text))
+            # print(u'Replying: ' + answer)
             print('####################')
-            api.update_status(
-                status=u'@{user} {answer}'.format(user=username, answer=answer),
-                in_reply_to_status_id=data.get('id'))
+            print('####################')
+            # Generating similar answers causes bot protection
+            # api.update_status(
+            #     status=u'@{user} {answer}'.format(user=username, answer=answer),
+            #     in_reply_to_status_id=data.get('id'))
 
         return True
 
     def on_error(self, status):
         print(status)
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(CLIENT_TOKEN, CLIENT_SECRET)
-api = tweepy.API(auth)
-
-l = StdOutListener()
-stream = tweepy.Stream(auth, l)
-
 while True:
     try:
-        stream.filter(track=["why", "Because", "@KakaruBot"],
-                      languages=['de', 'en'],
-                      follow=[])
-    except TweepError as e:
+        auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+        auth.set_access_token(CLIENT_TOKEN, CLIENT_SECRET)
+        api = tweepy.API(auth)
+
+        l = StdOutListener()
+        stream = tweepy.Stream(auth, l)
+
+        while True:
+            try:
+                stream.filter(track=["why", "Because", "@KakaruBot"],
+                              languages=['de', 'en'],
+                              follow=[])
+            except TweepError as e:
+                print(str(e))
+    except Exception as e:
         print(str(e))
+        print('Caught exception. Restarting in 10 seconds...')
+        time.sleep(10)
